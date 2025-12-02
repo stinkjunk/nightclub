054facb51499e47ec807ceb8fb0dff582056bf24
stinkjunk <magnuskrahl2@gmail.com>
2025-12-02 14:51:37 +0100

    relocated assets folder and it's contents into public/

--- src/app/page.js
+++ src/app/page.js
@@ -64,12 +64,21 @@
 //   );
 // }
 
+import Image from "next/image";
+
 export default function Home() {
   return (
     <main className="pattern-sm
     px-4 py-2">
       <h1 className="text-5xl">Ubuntu Font <span className="font-bold colActive">Ubuntu Font</span></h1>
       <p>Test</p>
+      <Image
+        className="w-1/3 py-5"
+        src="/assets/content-img/gallery5_big.jpg"
+        alt="test image"
+        width={800}
+        height={600}
+      />
     </main>
   );
 }
