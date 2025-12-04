4271d632346a5439cbe62325bf38d11421726038
stinkjunk <magnuskrahl2@gmail.com>
2025-12-04 13:47:37 +0100

    tilfældigt hero billede tilføjet

--- src/app/components/layout/Hero.jsx
+++ src/app/components/layout/Hero.jsx
@@ -7,16 +7,29 @@ import Header from "./header/Header";
 export default function Hero() {
   const pathname = usePathname();
   if (pathname == "/") {
+    const heroImg = Math.random() < 0.5 ? "/assets/bg/header_bg_1.jpg" : "/assets/bg/header_bg_2.jpg"; //per default genererer Math.random() et tilfældigt
+    //decimal tal mellem 0 og 1 - så der er 50% chance for hver herobillede at blive valgt
+    console.log(heroImg);
     return (
       <>
         <section
           className="h-screen flex flex-col absolute
-        bg-red-500 w-full
+        w-full
         "
         >
-          <p className="text-5xl mb-auto">
+          <Image
+            src={heroImg}
+            alt="hero"
+            className="w-full h-full object-cover"
+            fill
+          />
+          {/*billedet "hopper" visuelt anden frame efter første render - måske noget CSS som bliver sat på efter?*/}
+          {/*noget med react eller next i dev mode gør at billedet bliver renderet to gange, så giver mig hydration errors -
+          something something "strict mode" - burde ikke være et problem når et rigtigt buid er deployet. forhåbentligt*/}
+
+          {/* <p className="text-5xl mb-auto">
             Dummy Hero Component {"(Conditional)"}
-          </p>
+          </p> */}
         </section>
         {/*spacer element*/}
         <div className="h-[calc(100vh-var(--headerHeight))]"></div>
