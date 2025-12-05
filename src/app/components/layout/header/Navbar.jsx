fde63b90e5d55877ac89f3ba1ef48f9ef987b174
stinkjunk <magnuskrahl2@gmail.com>
2025-12-05 12:01:34 +0100

    "Pink streg" tilføjet til navigation

--- src/app/components/layout/header/Navbar.jsx
+++ src/app/components/layout/header/Navbar.jsx
@@ -3,43 +3,121 @@
 import { usePathname } from "next/navigation";
 import Link from "next/link";
 import Burger from "./Burger";
+import Image from "next/image";
 
 export default function Navbar() {
   const pathname = usePathname();
   return (
-  <>
-    <nav
-    className="hidden md:block"
-    > {/*Inefficient, places unnecessary elements in DOM when not needed. I don't think it's worth my time to figure
+    <>
+      <nav className="hidden md:block">
+        {" "}
+        {/*Inefficient, places unnecessary elements in DOM when not needed. I don't think it's worth my time to figure
     out true conditional rendering for this, though*/}
-      <ul className="h-full
+        <ul
+          className="h-full
       flex gap-10 items-center
-      ">
-        <li>
-          <Link href="/"
-          className={`uppercase text-xl ${pathname == "/" ? "isCurrentPage" : ""}`}
-          >Home</Link>
-        </li>
-        <li>
-          <Link href="/book_table"
-          className={`uppercase text-xl ${pathname == "/book_table" ? "isCurrentPage" : ""}`}
-          >Book Table</Link>
-        </li>
-        <li>
-          <Link href="/blog"
-          className={`uppercase text-xl ${pathname == "/blog" ? "isCurrentPage" : ""}`}
-          >Blog</Link>
-        </li>
-        <li>
-          <Link href="/contact_us"
-          className={`uppercase text-xl ${pathname == "/contact_us" ? "isCurrentPage" : ""}`}
-          >Contact Us</Link>
-        </li>
-      </ul>
-    </nav>
-    <Burger
-    sentClass="md:hidden"
-    />
-  </>
+      "
+        >
+          <li>
+            <Link
+              href="/"
+              className={`uppercase text-xl ${
+                pathname == "/" ? "isCurrentPage" : ""
+              }`}
+            >
+              Home
+            </Link>
+            {pathname == "/" && (
+              <div className="grid grid-cols-[1fr_1fr] w-full">
+                <div className="col-start-2">
+                  <Image
+                    src="/assets/bottom_line2.png"
+                    alt="Underline"
+                    width={150}
+                    height={12}
+                    className="absolute -translate-x-1/2"
+                  ></Image>
+                </div>
+              </div>
+            )}
+          </li>
+          <li>
+            <Link
+              href="/book_table"
+              className={`uppercase text-xl ${
+                pathname == "/book_table" ? "isCurrentPage" : ""
+              }`}
+            >
+              Book Table
+            </Link>
+
+            {pathname == "/book_table" && (
+              <div className="grid grid-cols-[1fr_1fr] w-full">
+                <div className="col-start-2">
+                  <Image
+                    src="/assets/bottom_line2.png"
+                    alt="Underline"
+                    width={150}
+                    height={12}
+                    className="absolute -translate-x-1/2"
+                  />
+                </div>
+              </div>
+            )}
+          </li>
+
+          <li>
+            <Link
+              href="/blog"
+              className={`uppercase text-xl ${
+                pathname == "/blog" ? "isCurrentPage" : ""
+              }`}
+            >
+              Blog
+            </Link>
+
+            {pathname == "/blog" && (
+              <div className="grid grid-cols-[1fr_1fr] w-full">
+                <div className="col-start-2">
+                  <Image
+                    src="/assets/bottom_line2.png"
+                    alt="Underline"
+                    width={150}
+                    height={12}
+                    className="absolute -translate-x-1/2"
+                  />
+                </div>
+              </div>
+            )}
+          </li>
+
+          <li>
+            <Link
+              href="/contact_us"
+              className={`uppercase text-xl ${
+                pathname == "/contact_us" ? "isCurrentPage" : ""
+              }`}
+            >
+              Contact Us
+            </Link>
+
+            {pathname == "/contact_us" && (
+              <div className="grid grid-cols-[1fr_1fr] w-full">
+                <div className="col-start-2">
+                  <Image
+                    src="/assets/bottom_line2.png"
+                    alt="Underline"
+                    width={150}
+                    height={12}
+                    className="absolute -translate-x-1/2"
+                  />
+                </div>
+              </div>
+            )}
+          </li>
+        </ul>
+      </nav>
+      <Burger sentClass="md:hidden" />
+    </>
   );
 }
