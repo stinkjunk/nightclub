4b4fc79b3040ccdd1833005249a8d6392db62a5a
stinkjunk <magnuskrahl2@gmail.com>
2025-12-08 22:17:57 +0100

    State sat op på EventsContent komponent

--- src/app/(pages)/home/events/EventsContent.jsx
+++ src/app/(pages)/home/events/EventsContent.jsx
@@ -1,20 +1,34 @@
 "use client";
+import { useState } from "react";
 
 import Title from "@/app/components/global/Title";
 import Image from "next/image";
 
 export default function EventsContent({ events }) {
-  const entries = Array.isArray(events) ? events : [];
+  const [currentSlide, setCurrentSlide] = useState(1);
+  //   const entries = Array.isArray(events) ? events : [];
+  const entries = events;
+  
+  //split entries into pairs
+  const entryPairs = [];
+  for (let i = 0; i < entries.length; i += 2) {
+    entryPairs.push(entries.slice(i, i + 2));
+  } //AI
+  console.log("Paired Entries: ", entryPairs);
 
   return (
-    <section 
-    // className="h-screen"
+    <section
+      className="h-screen flex flex-col
+    bg-[url('/assets/bg/slider_bg_overlay.png')]
+    bg-cover
+    bg-center
+    bg-no-repeat
+    "
     >
       <Title title="Events of the Month" />
       {/* Testing Code: */}
-      {entries.map(
+      {/* {entries.map(
         ({ id, title, description: desc, date, asset, location }) => (
-          //map all keys in event object to see what data is available
           <div key={id ?? title}>
             <p>{title}</p>
             <p>{desc}</p>
@@ -35,7 +49,29 @@ export default function EventsContent({ events }) {
             )}
           </div>
         )
-      )}
+      )} */}
+
+      <div className="flex-grow"></div>
+      <div
+        className="h-30
+      flex items-center justify-center gap-4
+      "
+      >
+        {entryPairs.map((pair, index) => {
+          const slide = index + 1;
+          return (
+            <button
+              className={`h-5 aspect-square cursor-pointer ${
+                currentSlide === slide
+                  ? "bg-[var(--active)]"
+                  : "bg-[var(--text)]"
+              }`}
+              key={index}
+              onClick={() => setCurrentSlide(slide)}
+            ></button>
+          );
+        })}
+      </div>
     </section>
   );
 }
