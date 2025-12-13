435bf2d2f86996e0564b5cd041d5ec9e4a580c96
stinkjunk <magnuskrahl2@gmail.com>
2025-12-13 14:44:43 +0100

    Vælg bord funktionalitet implementeret;

--- src/app/(pages)/book_table/Table.jsx
+++ src/app/(pages)/book_table/Table.jsx
@@ -1,7 +1,12 @@
 "use client";
 import Image from "next/image";
+import Link from "next/link";
+import { useSearchParams } from "next/navigation";
+
 export default function Table(props) {
   const table = props.table;
+  const searchParams = useSearchParams();
+  const currentSelected = searchParams.get("selected");
   //   let tableImg;
   //   if (table.size === 1) {
   //     tableImg = "/assets/table/table_1.png";
@@ -10,8 +15,16 @@ export default function Table(props) {
   //table pngs er alle 230 x 150
   // aspect ratio 23:15
 
+  
   const reserved = false; // hardcoded placeholder værdi
-  const reservedFilter = reserved ? "bg-red-500 mix-blend-color opacity-75" : "";
+  const reservedFilter = reserved
+    ? "bg-red-500 opacity-75"
+    : "";
+  
+  const selected = currentSelected === String(table.id);
+  const selectedFilter = selected
+    ? "bg-green-500 opacity-75"
+    : "";
 
   const tableImg = `/assets/table/table_${table.size}.png`;
   const maskClass =
@@ -21,15 +34,30 @@ export default function Table(props) {
       ? "[mask-image:url('/assets/table/table_2.png')]"
       : "[mask-image:url('/assets/table/table_3.png')]";
 
+  const href = (() => {
+    if (reserved) return "#";
+    const isSelected = currentSelected === String(table.id);
+    return isSelected ? "/book_table" : `/book_table?selected=${table.id}`;
+  })();
+
   return (
-    <div
+    /*     <div
       className={`${maskClass} relative table-mask grid
-    w-full aspect-[23/15] rotate-90 my-25 scale-150
+    w-full aspect-[23/15] rotate-90 my-10 scale-150
+    sm:my-25
     md:rotate-0 md:my-3 md:scale-100 overflow-hidden
+    `}
+    > */
+    <div
+      className={`${maskClass} relative table-mask grid ${reserved || selected ? 'active' : ''}
+    w-full aspect-[23/15] rotate-90 my-10 scale-150
+    my-[13vw]
+    md:rotate-0 md:my-3 md:scale-100 overflow-hidden
+
     `}
     >
       {/* table {table.id}, size {table.size} */}
-      <div className={`[grid-area:1/1] z-1 ${reservedFilter}`}></div>
+      <div className={`[grid-area:1/1] z-1 mix-blend-color transition-all duration-250 ${reservedFilter} ${selectedFilter}`}></div>
       <Image
         src={tableImg}
         alt={`Table ${table.id}`}
@@ -43,9 +71,15 @@ export default function Table(props) {
       md:rotate-0
       "
       >
-        <button
-        className={`p-4 ${reserved ? 'cursor-not-allowed text-black' : 'cursor-pointer'}`}
-        >{table.id}</button>
+        <Link
+          href={href}
+          scroll={false}
+          className={`px-8 py-4 rounded-full hoverChild ${
+            reserved ? "cursor-not-allowed text-black" : "cursor-pointer"
+          }`}
+        >
+          {table.id}
+        </Link>
       </div>
     </div>
   );
--- src/app/(pages)/book_table/Tables.jsx
+++ src/app/(pages)/book_table/Tables.jsx
@@ -1,5 +1,6 @@
 import Table from "./Table";
 
+
 export default function Tables(props) {
   const tables = props.tables;
   return (
@@ -7,10 +8,10 @@ export default function Tables(props) {
       className="
       grid grid-cols-3
       
-      px-10 pb-10 
-      md:px-30 md:pb-15
+      px-10 py-10 
+      md:px-30 md:py-15
       lg:grid-cols-5 
-      xl:px-40 xl:pb-20 
+      xl:px-40 xl:py-20 
       "
     >
       {/* <p>Work in Progress</p> */}
